c3f26fb7aa60dd56df5afa863d224ac83dc7ae27
merzougao <merzougao@gmail.com>
2024-07-11 17:41:15 +0000

    Structural brainstorm on Holes in Context. The current approach involve using binary tree with values stored in the leafs.

--- Lean/STLC/STLC/NALC.lean
+++ Lean/STLC/STLC/NALC.lean
@@ -30,17 +30,59 @@ notation:max t₁"{"t₂"}" => Term.app t₁ t₂
 #check (λ←[0].5){4}
 #check (λ→[0].5){4}
 
+-- Need to define PreCtx as a tree, can't use mathlib because we need to store
+-- values in leaf nodes.
 
-inductive Ctx : Type
-  | nil : Ctx
-  | cons_typ : Typ → Ctx
-  | cons : (Tree Typ) → Ctx → Ctx
+inductive PreCtx : Type
+  | leaf : Term → Typ → PreCtx
+  | cons : (left : PreCtx) → (right: PreCtx) → PreCtx
 
-notation:max "[]" => Ctx.nil
-notation:max Γ"["A"]" => Ctx.cons Γ A
+def left : PreCtx → PreCtx := by
+  intro Γ
+  cases Γ
+  case leaf a A => exact PreCtx.leaf a A
+  case cons l r => exact l
 
-inductive Deduction : Ctx → Term → Typ → Type
-  | id : Deduction [] (λ[x].Term.var x) A
-  | cut {Γ : Tree Typ}{A B : Typ}: (Deduction Γ[Ctx.cons_typ A] b B)
-                                    → Deduction Δ a A
-                                    → Deduction Γ[Δ] λ γ. b(γ(a(δ))) B
+def right : PreCtx → PreCtx := by
+  intro Γ
+  cases Γ
+  case leaf a A => exact PreCtx.leaf a A
+  case cons l r => exact r
+
+notation:max "["a"∶"A"]" => PreCtx.leaf a A
+
+instance : Append (PreCtx) where
+  append := PreCtx.cons
+
+inductive Path : Type
+  | dest : Path
+  | left : Path → Path
+  | right : Path → Path
+
+inductive ValidPath : PreCtx → Path → Prop
+  | dest (a : Term) (A : Typ) : ValidPath (PreCtx.leaf a A) dest
+  | cons (l r : PreCtx): ValidPath (l ++ r) p
+
+structure Ctx where
+  tree : PreCtx
+  path : Path
+  valid : ValidPath tree path
+
+def Γ : Ctx := Ctx.mk [0∶base] Path.dest (ValidPath.dest 0 base)
+
+def Subst : Ctx → Term → Typ → Ctx := by
+  intro Γ a A
+  cases Γ
+  case mk t₀ p₀ v₀ =>
+    induction p₀
+    case dest => exact Ctx.mk [a∶A] Path.dest (ValidPath.dest a A)
+    case left pl iH =>
+      exact (Ctx.mk (left t₀) pl (ValidPath.cons (left t₀) (right t₀) pl))
+
+
+
+-- inductive Deduction : Ctx → Term → Typ → Type
+--   | id : Deduction [] (λ[x].Term.var x) A
+--   | cut {Γ : Tree Typ}{A B : Typ}: (Deduction Γ[Ctx.cons_typ A] b B)
+--                                     → Deduction Δ a A
+--                                     → Deduction Γ[Δ] λ γ. b(γ(a(δ))) B
