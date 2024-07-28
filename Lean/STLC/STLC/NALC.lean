c43df2cc44bde504ceb7f467f3d7d6f1fd24e71b
merzougao <merzougao@gmail.com>
2024-07-28 18:57:12 +0000

    Inductive definition of Trees with holes

--- Lean/STLC/STLC/NALC.lean
+++ Lean/STLC/STLC/NALC.lean
@@ -1,133 +1,73 @@
 import Mathlib.Init.Data.List.Basic
-import Mathlib.Data.Tree.Basic
-
-open List
-open Tree
+--import Mathlib.Data.Tree.Basic
 
 inductive Typ : Type
   | base : Typ
-  | arrow : Typ → Typ → Typ
-
-export Typ (base)
-notation A"->"B => Typ.arrow A B
-
-def TypToString : Typ → String := by
-  intro A
-  cases A
-  case base => exact "base"
-  case arrow A₀ A₁ => exact TypToString A₀ ++ "->" ++ TypToString A₁
+  | leftarrow : Typ → Typ → Typ
+open Typ
 
 inductive Term : Type
   | var : ℕ → Term
   | abs : ℕ → Term → Term
   | app : Term → Term → Term
+  | leftarrow : Term → TreeTerm → Term
 
-def TermToString : Term → String := by
-  intro t
+open Term
+
+def subst : ℕ → Term → Term → Term := by
+  intro n u t
   cases t
-  case var m => exact "x"++toString m
-  case abs x p => exact "λ" ++ "x"++ (toString x) ++ ". " ++ (TermToString p)
-  case app t₀ t₁ => exact "(" ++ TermToString t₀ ++ ")(" ++ TermToString t₁ ++ ")"
+  case var m => exact if n = m then u else Term.var m
+  case abs x p => exact if n = x then (abs x p) else (abs x (subst n u p))
+  case app t₀ t₁ => exact app (subst n u t₀) (subst n u t₁)
 
-instance : ToString (Term) where
-  toString := TermToString
+notation:max t"["u"//"x"]" => subst x u t
 
-instance : ToString (Typ) where
-  toString := TypToString
+-- Need this for dependencies between trees --
+inductive TreeTyp : Type
+  | hole : TreeTyp
+  | leaf : Typ → TreeTyp
+  | cons : TreeTyp → TreeTyp → TreeTyp
 
-instance : OfNat Term n where
-  ofNat := Term.var n
+open TreeTyp
 
-notation:max "λ["x"]."t => Term.abs x t
--- notation:max "λ←["x"]."t => Term.abs_left x t
--- notation:max "λ→["x"]."t => Term.abs_right x t
-notation:max t₁"{"t₂"}" => Term.app t₁ t₂
+inductive TreeTerm : Type
+  | nil : TreeTerm
+  | hole : TreeTerm
+  | leaf : Term → TreeTerm
+  | cons : TreeTerm → TreeTerm → TreeTerm
+open TreeTerm
 
--- #check (λ←[0].5){4}
--- #check (λ→[0].5){4}
+inductive βRed : TreeTerm → TreeTerm → Type
+  | id_left : βRed (cons nil t) t
+  | id_right : βRed (cons t nil) t
 
-#check (λ[0].5){4}
+inductive βClosure : TreeTerm → TreeTerm → Type
+  | init : βRed t q → βClosure t q
+  | rfl : βClosure t t
+  | trans : βClosure t₁ t₂ → βClosure t₂ t₃ → βClosure t₁ t₃
 
+--\ t--
+notation t"▸"q => βClosure t q
 
--- Need to define PreCtx as a tree, can't use mathlib because we need to store
--- values in leaf nodes.
-
-inductive PreCtx : Type
-  | leaf : Term → Typ → PreCtx
-  | cons : (left : PreCtx) → (right: PreCtx) → PreCtx
-
-def PreCtxToString : PreCtx → String := by
-  intro Γ
+def TreeTermSub : TreeTerm → TreeTerm → TreeTerm := by
+  intro Γ Δ
   cases Γ
-  case leaf a A => exact toString a ++ ":" ++ toString A
-  case cons l r => exact "(" ++ PreCtxToString l ++","++ PreCtxToString r ++ ")"
+  case nil => exact nil
+  case hole => exact Δ
+  case leaf t => exact leaf t
+  case cons Γ₁ Γ₂ => exact cons (TreeTermSub Γ₁ Δ) (TreeTermSub Γ₂ Δ)
 
-instance : ToString (PreCtx) where
-  toString := PreCtxToString
+def TreeTypSub : TreeTyp → TreeTyp → TreeTyp := by
+  intro T Q
+  cases T
+  case hole => exact Q
+  case leaf t => exact leaf t
+  case cons T₁ T₂ => exact cons (TreeTypSub T₁ Q) (TreeTypSub T₂ Q)
 
+inductive TreeDeduction : TreeTerm → TreeTerm → TreeTyp → Type
+  | hole : TreeDeduction hole hole hole
+  | leaf : (t : Term) → (T : Typ) → TreeDeduction nil (leaf t) (leaf T)
+  | cons : (t = nil ∨ q = nil) → TreeDeduction  t t₁ T₁ → TreeDeduction  q q₁ Q₁ → TreeDeduction (cons t q) (cons t₁ q₁) (cons T₁ Q₁)
 
-def left : PreCtx → PreCtx := by
-  intro Γ
-  cases Γ
-  case leaf a A => exact PreCtx.leaf a A
-  case cons l r => exact l
-
-def right : PreCtx → PreCtx := by
-  intro Γ
-  cases Γ
-  case leaf a A => exact PreCtx.leaf a A
-  case cons l r => exact r
-
-notation:max "["a"∶"A"]" => PreCtx.leaf a A
-
-instance : Append (PreCtx) where
-  append := PreCtx.cons
-
-def Γ' : PreCtx := [0∶base] ++ ([2∶base] ++ [5∶base->base])
-#eval Γ'
-
-
-inductive Path : Type
-  | nil : Path
-  | dest : Path
-  | left : Path → Path
-  | right : Path → Path
-
-inductive ValidPath : PreCtx → Path → Prop
-  | nil : ValidPath Γ Path.nil
-  | dest (a : Term) (A : Typ) : ValidPath (PreCtx.leaf a A) dest
-  | cons (l r : PreCtx): ValidPath (l ++ r) p
-
-notation:max p"⊆"Γ => ValidPath Γ p
-
-def Subst : PreCtx → Path → Term → Typ → PreCtx := by
-  intro Γ p a A
-  cases p
-  case nil => exact Γ
-  case dest => exact [a∶A]
-  case left pl =>
-    cases Γ
-    case leaf a₀ A₀ => exact [a₀∶A₀]
-    case cons l r => exact (Subst l pl a A) ++ r
-  case right pr =>
-    cases Γ
-    case leaf a₀ A₀ => exact [a₀∶A₀]
-    case cons l r => exact l ++ (Subst r pr a A)
-
-notation:max Γ"←["a"∶"A"]"p => Subst Γ p a A
-
-#eval [0∶base]←[4∶base](Path.left Path.dest)
-#eval [0∶base]←[4∶base]Path.dest
-#eval ([0∶base] ++ ([2∶base] ++ [5∶base->base]))←[4∶base](Path.left Path.dest)
-#eval ([0∶base] ++ ([2∶base] ++ [5∶base->base]))←[4∶base](Path.right Path.dest)
-#eval ([0∶base] ++ ([2∶base] ++ [5∶base->base]))←[4∶base](Path.right (Path.right Path.dest))
-
-
-
-
-
--- inductive Deduction : Ctx → Term → Typ → Type
---   | id : Deduction [] (λ[x].Term.var x) A
---   | cut {Γ : Tree Typ}{A B : Typ}: (Deduction Γ[Ctx.cons_typ A] b B)
---                                     → Deduction Δ a A
---                                     → Deduction Γ[Δ] λ γ. b(γ(a(δ))) B
+notation Γ "⊩" t":"T => TreeDeduction Γ t T
